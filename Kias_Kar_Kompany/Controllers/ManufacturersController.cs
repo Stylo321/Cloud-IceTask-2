4b434d0ec70ec2b1bb1da2716966fbe5fa19afbe
Stylo <165194260+Stylo321@users.noreply.github.com>
2026-04-21 20:36:29 +0200

    Refactor ManufacturersController for improved structure

--- Kias_Kar_Kompany/Controllers/ManufacturersController.cs
+++ Kias_Kar_Kompany/Controllers/ManufacturersController.cs
@@ -1,51 +1,90 @@
-﻿using Kias_Kar_Kompany.Data;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Kias_Kar_Kompany.Data;
 using Kias_Kar_Kompany.Models;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Kias_Kar_Kompany.Controllers
+public class ManufacturersController : Controller
 {
-    public class ManufacturersController : Controller
+    private readonly Kias_Kar_KompanyContext _context;
+
+    public ManufacturersController(Kias_Kar_KompanyContext context)
     {
+        _context = context;
+    }
 
-        private readonly Kias_Kar_KompanyContext _context;
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Manufacturer.ToListAsync());
+    }
 
-        public ManufacturersController(Kias_Kar_KompanyContext context)
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var manufacturer = await _context.Manufacturer
+            .FirstOrDefaultAsync(m => m.Id == id);
+
+        if (manufacturer == null) return NotFound();
+
+        return View(manufacturer);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Manufacturer manufacturer)
+    {
+        if (ModelState.IsValid)
         {
-            _context = context;
+            _context.Add(manufacturer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+        return View(manufacturer);
+    }
 
-        // GET: ManufacturersController
-        public async Task<ActionResult> Index()
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var manufacturer = await _context.Manufacturer.FindAsync(id);
+        if (manufacturer == null) return NotFound();
+
+        return View(manufacturer);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(int id, Manufacturer manufacturer)
+    {
+        if (id != manufacturer.Id) return NotFound();
+
+        if (ModelState.IsValid)
         {
-            return View(await _context.Manufacturer.ToListAsync());
+            _context.Update(manufacturer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+        return View(manufacturer);
+    }
 
-        
-        // GET: ManufacturersController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
 
-        // POST: ManufacturersController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(Manufacturer manufacturer)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(manufacturer);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            
-            return View(manufacturer);
-            
-        }
+        var manufacturer = await _context.Manufacturer.FindAsync(id);
+        if (manufacturer == null) return NotFound();
 
-        // GET: ManufacturersController/Edit/5
-        
+        return View(manufacturer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var manufacturer = await _context.Manufacturer.FindAsync(id);
+        _context.Manufacturer.Remove(manufacturer);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
